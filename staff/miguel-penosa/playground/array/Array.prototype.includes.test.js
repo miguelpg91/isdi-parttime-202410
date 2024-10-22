536b987676bc4587daf8d2faffeda9d6675835cc
miguelpg91 <miguel.pgomez91@gmail.com>
2024-10-22 16:02:43 +0200

    Add tests for Array.prototype.includes method

---
+++ staff/miguel-penosa/playground/array/Array.prototype.includes.test.js
@@ -0,0 +1,13 @@
+var frutas = ["manzana", "banana", "naranja", "pera"];
+
+if (frutas.includes("banana")) {
+    console.log("¡Sí, tenemos bananas!");
+} else {
+    console.log("No hay bananas.");
+}
+
+if (frutas.includes("uva")) {
+    console.log("¡Tenemos uvas!");
+} else {
+    console.log("No hay uvas.");
+}
