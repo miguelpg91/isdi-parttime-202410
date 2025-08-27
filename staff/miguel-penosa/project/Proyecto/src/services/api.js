3f9838961a02b852e14c53c6154966886bfeb1dc
miguelpg91 <miguel.pgomez91@gmail.com>
2025-08-27 03:05:30 +0200

    Creating the frontend of thefinal project #7

---
+++ staff/miguel-penosa/project/Proyecto/src/services/api.js
@@ -0,0 +1,8 @@
+// Aquí pondremos funciones para conectar con el backend (API)
+
+export async function fetchData() {
+    // Ejemplo de fetch:
+    // const response = await fetch('http://localhost:4000/api/data');
+    // const data = await response.json();
+    // return data;
+}
